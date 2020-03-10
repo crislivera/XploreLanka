7432cd1ecb87283ac0873f3efbac5242c0e6f8ed
Has_i <hasani88d@gmail.com>
2020-03-10 13:04:49 +0530

    removed information provider from backend controller

--- src/main/java/com/example/backend/controller/backendController.java
+++ src/main/java/com/example/backend/controller/backendController.java
@@ -2,7 +2,6 @@ package com.example.backend.controller;
 
 import com.example.backend.DBConnection.dbConnection;
 import com.example.backend.Models.Admin;
-import com.example.backend.Models.Information_Provider;
 import com.example.backend.Models.User;
 
 import java.sql.SQLException;
@@ -22,9 +21,7 @@ public class backendController {
         return con.registerUser(obj);
     }
 
-    public boolean registerIP(Information_Provider obj) throws SQLException {
-        return con.registerIP(obj);
-    }
+
 
     public Admin loginAdmin(String username,String password) throws SQLException {
         return con.loginAdmin(username,password);
@@ -34,8 +31,6 @@ public class backendController {
         return con.loginUser(username,password);
     }
 
-    public Information_Provider loginIP(String username, String password) throws SQLException {
-        return con.loginIP(username,password);
-    }
+
 
 }

b7157aa43de5324c742687790539035661f73500
ruferdz <ge.rusiru@gmail.com>
2020-03-10 12:49:15 +0530

    user function API class added

---
+++ src/main/java/com/example/backend/APIs/userFunction_API.java
@@ -0,0 +1,33 @@
+package com.example.backend.APIs;
+
+import com.example.backend.Models.Admin;
+import com.example.backend.Models.Trip_Schedule;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+
+public class userFunction_API {
+    @PostMapping("/saveSchedule")
+    @ResponseBody
+    public void saveSchedule(@RequestBody Trip_Schedule schedule) throws SQLException {
+
+    }
+
+    @PostMapping("/getSchedule")
+    @ResponseBody
+    public void getSchedule(@RequestParam String  id) throws SQLException {
+
+    }
+
+    @PostMapping("/editSchedule")
+    @ResponseBody
+    public void editSchedule(@RequestParam String  id) throws SQLException {
+
+    }
+
+    @PostMapping("/deleteSchedule")
+    @ResponseBody
+    public void deleteSchedule(@RequestParam String  id) throws SQLException {
+
+    }
+}

983dc069da0c4e1238211dd09dd0d07b8091801a
Has_i <hasani88d@gmail.com>
2020-04-29 03:37:32 +0530

    PlaceSession model created

---
+++ Version 2.0/src/main/java/com/example/backend/Models/PlaceSession.java
@@ -0,0 +1,36 @@
+package com.example.backend.Models;
+
+public class PlaceSession {
+
+    String userID;
+    String placeID;
+
+    public PlaceSession(String userID, String placeID) {
+        this.userID = userID;
+        this.placeID = placeID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getPlaceID() {
+        return placeID;
+    }
+
+    public void setPlaceID(String placeID) {
+        this.placeID = placeID;
+    }
+
+    @Override
+    public String toString() {
+        return "TempTrip{" +
+                "userID='" + userID + '\'' +
+                ", placeID='" + placeID + '\'' +
+                '}';
+    }
+}
